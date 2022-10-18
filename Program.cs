0330b8d83cfcb5c94b00af364f991d9613e96942
Kiryl_Bakhur <bahur201554@gmail.com>
2022-10-18 21:58:37 +0300

    Add cycle

--- Program.cs
+++ Program.cs
@@ -4,3 +4,12 @@
             M = stroka.Split(' ');
             var result = new string[M .Length];
             var realSize = 0;
+            foreach (var value in M )
+            {
+
+                if (value.Length <= 3)
+                {
+                    result[realSize] = value;
+                    realSize++;
+                }
+            }
