b80a0c7317c6ee0e977143b0537dafbbb456bcad
Kiryl_Bakhur <bahur201554@gmail.com>
2022-10-18 21:57:35 +0300

    Add 2 string

--- Program.cs
+++ Program.cs
@@ -2,3 +2,5 @@
             Console.WriteLine("Введите массив строк через пробел");
             string stroka = Console.ReadLine();
             M = stroka.Split(' ');
+            var result = new string[M .Length];
+            var realSize = 0;
